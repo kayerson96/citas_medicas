e78db458d5d6886ad7ff86f109779a112a20a7c6
kayerson96 <108358349+kayerson96@users.noreply.github.com>
2022-09-19 10:22:15 -0500

    ya la factura funciona

--- obtenerFactura.js
+++ obtenerFactura.js
@@ -1,11 +1,11 @@
-Function facturaPDF() {
-    let nombre = document.getElementById('nombrecompleto').value;
+function facturaPDF() {
+    let nombre = document.getElementById('nombre_completo').value;
     let tipo_documento= document.getElementById('tipodedocumento').value;
     let numero_documento = document.getElementById('numero_documento').value;
     let motivo_hospitalizacion = document.getElementById('hospitalizacion').value;
     let fechainicio = document.getElementById('fechainicio').value;
     let fechaFin = document.getElementById('fechafin').value;
- 
+
     
     let siSisben = document.getElementById('esteesSi');
     if (siSisben.checked == true){
@@ -13,4 +13,22 @@ Function facturaPDF() {
     }else{
         siSisben = "No";
     }
+
+
+let documentoPDF = new jsPDF();
+documentoPDF.setFontSize(20);
+documentoPDF.text(70, 15, 'Factura por Hospitalización');
+documentoPDF.setFontSize(12);
+documentoPDF.text(20, 30, 'Fecha: ' + new Date());
+documentoPDF.setFontSize(15);
+documentoPDF.text(70, 40, 'Paciente: ' + nombre);
+documentoPDF.setFontSize(10);
+documentoPDF.text(20, 50, 'Tipo de Documento: ' + tipo_documento);
+documentoPDF.text(20, 60, 'Número de documento: ' + numero_documento);
+documentoPDF.text(20, 70, 'Motivo de Hospitalización: ' + motivo_hospitalizacion);
+documentoPDF.text(20, 80, 'Cuenta con Sisben: ' + siSisben);
+documentoPDF.text(20, 90, 'Fecha de Ingreso ' + fechainicio); 
+documentoPDF.text(20, 100, 'Fecha de Salida ' + fechaFin);   
+documentoPDF.save('Factura_Hospitalizacion');
+
 }
