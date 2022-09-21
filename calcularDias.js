5f22da17d8c84bc43a859237201f64640197f974
kayerson96 <108358349+kayerson96@users.noreply.github.com>
2022-09-21 18:27:49 -0500

    I think that finish the part about pay

--- calcularDias.js
+++ calcularDias.js
@@ -1,6 +1,6 @@
 function calcularDias() {
-    let fechainicio = new Date(document.getElementById('fechainicio').value);
-    let fechafin =  new Date(document.getElementById('fechafin').value);
+    var fechainicio = new Date(document.getElementById('fechainicio').value);
+    var fechafin =  new Date(document.getElementById('fechafin').value);
     if (fechainicio >= fechafin){
         alert("la información indicada no es correcto, validar fecha de ingreso")
         // fechainicio.value="";
@@ -8,15 +8,32 @@ function calcularDias() {
         document.getElementById("fechainicio").value="";
         document.getElementById("fechafin").value="";
     }else{
-        let fechaResultado= fechafin - fechainicio;
-        let days = Math.floor((fechaResultado)/(1000*60*60*24))+1;
-        alert(days);
+        var fechaResultado= fechafin - fechainicio;
+        var days = Math.floor((fechaResultado)/(1000*60*60*24))+1;
+        alert(days + " de Hospitalización");
         return days;
     }
+
+    
 }
-function calcularValor (days) {
+function calcularValor () {
+let esteesA = document.getElementById("esteesA");
+let esteesB = document.getElementById("esteesB");
+
+let days= calcularDias();
     let valor = 75000;
+    let descuentoSisben= 0;
     let subtotal = valor*days;
-    alert(subtotal);
-    return subtotal;
+        if (esteesA.checked == true || esteesB.checked == true){    
+            descuentoSisben=35
+         }
+         let Total= ( subtotal-(subtotal*35)/100);
+         console.log(Total)
+
+    document.getElementById("espacio_dias").innerHTML ="Sub-Total "+subtotal + "COP";
+    alert("Sub-Total "+subtotal + "COP");
+    document.getElementById("totalGeneral").innerHTML ="Total a Pagar "+Total+ "COP";
+    return subtotal + Total; 
+
+
 }
