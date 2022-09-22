f42358d142517b204deb930dbe3da80d5c74aeb3
kayerson96 <108358349+kayerson96@users.noreply.github.com>
2022-09-21 22:13:05 -0500

    i'm modificate the facture

--- obtenerFactura.js
+++ obtenerFactura.js
@@ -6,13 +6,43 @@ function facturaPDF() {
     let fechainicio = document.getElementById('fechainicio').value;
     let fechaFin = document.getElementById('fechafin').value;
 
+
+
+    let GrupoA= document.getElementById('esteesA');
+    let GrupoB= document.getElementById('esteesB');
+    let GrupoC= document.getElementById('esteesC');
+    let GrupoD= document.getElementById('esteesD');
+    let grupoPertenece = 0;
+
     
     let siSisben = document.getElementById('esteesSi');
     if (siSisben.checked == true){
         siSisben = "Si";
+
+        if (GrupoA.checked == true){
+            grupoPertenece= "A con 35% de descuento";
+                
+        }else if(GrupoB.checked == true){
+                    grupoPertenece= "B con 35% de descuento";
+                        
+                }else if(GrupoC.checked == true){
+                            grupoPertenece= "C";
+                            
+                        }else if(GrupoD.checked == true){
+                                grupoPertenece= "D";}
+
+
+
     }else{
         siSisben = "No";
+        grupoPertenece = "No aplica"
     }
+
+
+
+                                    
+                                        
+    console.log(grupoPertenece)
 
 
 let documentoPDF = new jsPDF();
@@ -24,11 +54,14 @@ documentoPDF.setFontSize(15);
 documentoPDF.text(20, 40, 'Paciente: ' + nombre);
 documentoPDF.setFontSize(10);
 documentoPDF.text(20, 50, 'Tipo de Documento: ' + tipo_documento);
-documentoPDF.text(70, 50, 'Número de documento: ' + numero_documento);
-documentoPDF.text(20, 70, 'Motivo de Hospitalización: ' + motivo_hospitalizacion);
-documentoPDF.text(20, 80, 'Cuenta con Sisben: ' + siSisben);
-documentoPDF.text(20, 90, 'Fecha de Ingreso ' + fechainicio); 
-documentoPDF.text(20, 100, 'Fecha de Salida ' + fechaFin);   
+documentoPDF.text(90, 50, 'Número de documento: ' + numero_documento);
+documentoPDF.text(60, 60, 'Motivo de Hospitalización: ' + motivo_hospitalizacion);
+documentoPDF.text(20, 70, 'Cuenta con Sisben: ' + siSisben); 
+documentoPDF.text(80, 70, 'Grupo al que Pertenece: ' + grupoPertenece);
+documentoPDF.text(20, 80, 'Fecha de Ingreso ' + fechainicio); 
+documentoPDF.text(80, 80, 'Fecha de Salida ' + fechaFin); 
+documentoPDF.text(90, 0, 'Fecha de Salida ' + fechaFin); 
+
 documentoPDF.save('Factura_Hospitalizacion');
 
 }
