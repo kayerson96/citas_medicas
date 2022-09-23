9020b4bc6a59691307fc30f17382c96795997c68
kayerson96 <108358349+kayerson96@users.noreply.github.com>
2022-09-22 22:40:15 -0500

    factura y funciones funcional

--- calcularDias.js
+++ calcularDias.js
@@ -20,15 +20,14 @@ function calcularValor() {
 let esteesA = document.getElementById("esteesA");
 let esteesB = document.getElementById("esteesB");
 
-let days= calcularDias();
+var days= calcularDias();
     let valor = 75000;
     let descuentoSisben= 0;
-    let subtotal = valor*days;
+    var subtotal = valor*days;
         if (esteesA.checked == true || esteesB.checked == true){    
             descuentoSisben=35
          }
          Total= ( subtotal-(subtotal*35)/100);
-         console.log(Total)
 
     document.getElementById("espacio_dias").innerHTML="Sub-Total "+subtotal + "COP";
     alert("Sub-Total "+subtotal + "COP");
@@ -37,5 +36,78 @@ let days= calcularDias();
     document.getElementById("totalGeneral").innerHTML ="Total a Pagar "+Total+ "COP";
     return Total;
 
-   
+ 
+
+}
+
+
+function facturaPDF() {
+    let nombre = document.getElementById('nombre_completo').value;
+    let tipo_documento= document.getElementById('tipodedocumento').value;
+    let numero_documento = document.getElementById('numero_documento').value;
+    let motivo_hospitalizacion = document.getElementById('hospitalizacion').value;
+    let fechainicio = document.getElementById('fechainicio').value;
+    let fechaFin = document.getElementById('fechafin').value;
+    let subtotal = document.getElementById("espacio_dias");
+
+    let GrupoA= document.getElementById('esteesA');
+    let GrupoB= document.getElementById('esteesB');
+    let GrupoC= document.getElementById('esteesC');
+    let GrupoD= document.getElementById('esteesD');
+    let grupoPertenece = 0;
+
+    
+    let siSisben = document.getElementById('esteesSi');
+    if (siSisben.checked == true){
+        siSisben = "Si";
+
+        if (GrupoA.checked == true){
+            grupoPertenece= "A con 35% de descuento";
+                
+        }else if(GrupoB.checked == true){
+                    grupoPertenece= "B con 35% de descuento";
+                        
+                }else if(GrupoC.checked == true){
+                            grupoPertenece= "C";
+                            
+                        }else if(GrupoD.checked == true){
+                                grupoPertenece= "D";}
+
+
+
+    }else{
+        siSisben = "No";
+        grupoPertenece = "No aplica"
+    }
+    var resultadosemifinal = calcularDias()
+   subtotal1= 75000*resultadosemifinal;
+
+   var imgData = '../'
+
+let documentoPDF = new jsPDF();
+documentoPDF.setFontSize(30);
+documentoPDF.text(40, 15, 'Factura por Hospitalización');
+documentoPDF.setFontSize(12);
+documentoPDF.text(20, 30, 'Fecha: ' + new Date());
+documentoPDF.setFontSize(15);
+documentoPDF.setTextColor(8, 47, 86)
+documentoPDF.text(20, 40, 'Paciente: ' + nombre);
+documentoPDF.setTextColor(0, 0, 0)
+documentoPDF.setFontSize(10);
+documentoPDF.text(20, 50, 'Tipo de Documento: ' + tipo_documento);
+documentoPDF.text(90, 50, 'Número de documento: ' + numero_documento);
+documentoPDF.text(60, 60, 'Motivo de Hospitalización: ' + motivo_hospitalizacion);
+documentoPDF.text(20, 70, 'Cuenta con Sisben: ' + siSisben); 
+documentoPDF.text(80, 70, 'Grupo al que Pertenece: ' + grupoPertenece);
+documentoPDF.text(20, 80, 'Fecha de Ingreso ' + fechainicio); 
+documentoPDF.text(60, 160, 'Gracias por confiar tu Salud con Nosotros'); 
+documentoPDF.text(80, 80, 'Fecha de Salida ' + fechaFin); 
+documentoPDF.setFontSize(20);
+documentoPDF.setTextColor(129, 175, 221)
+documentoPDF.text(60, 140, 'Sub-Total: ' + subtotal1 + "COP"); 
+documentoPDF.setTextColor(8, 47, 86)
+documentoPDF.text(60, 150, 'Valor a pagar: ' + calcularValor()+"COP"); 
+documentoPDF.save('Factura_Hospitalizacion');
+documentoPDF.setTextColor(0, 0, 0)
+documentoPDF.setFontSize(10); 
 }
--- obtenerFactura.js
+++ obtenerFactura.js
@@ -1,66 +1,66 @@
 
 
-function facturaPDF() {
-    let nombre = document.getElementById('nombre_completo').value;
-    let tipo_documento= document.getElementById('tipodedocumento').value;
-    let numero_documento = document.getElementById('numero_documento').value;
-    let motivo_hospitalizacion = document.getElementById('hospitalizacion').value;
-    let fechainicio = document.getElementById('fechainicio').value;
-    let fechaFin = document.getElementById('fechafin').value;
-    let subtotal = document.getElementById("espacio_dias");
+// function facturaPDF() {
+//     let nombre = document.getElementById('nombre_completo').value;
+//     let tipo_documento= document.getElementById('tipodedocumento').value;
+//     let numero_documento = document.getElementById('numero_documento').value;
+//     let motivo_hospitalizacion = document.getElementById('hospitalizacion').value;
+//     let fechainicio = document.getElementById('fechainicio').value;
+//     let fechaFin = document.getElementById('fechafin').value;
+//     let subtotal = document.getElementById("espacio_dias");
 
-    let GrupoA= document.getElementById('esteesA');
-    let GrupoB= document.getElementById('esteesB');
-    let GrupoC= document.getElementById('esteesC');
-    let GrupoD= document.getElementById('esteesD');
-    let grupoPertenece = 0;
+//     let GrupoA= document.getElementById('esteesA');
+//     let GrupoB= document.getElementById('esteesB');
+//     let GrupoC= document.getElementById('esteesC');
+//     let GrupoD= document.getElementById('esteesD');
+//     let grupoPertenece = 0;
 
     
-    let siSisben = document.getElementById('esteesSi');
-    if (siSisben.checked == true){
-        siSisben = "Si";
+//     let siSisben = document.getElementById('esteesSi');
+//     if (siSisben.checked == true){
+//         siSisben = "Si";
 
-        if (GrupoA.checked == true){
-            grupoPertenece= "A con 35% de descuento";
+//         if (GrupoA.checked == true){
+//             grupoPertenece= "A con 35% de descuento";
                 
-        }else if(GrupoB.checked == true){
-                    grupoPertenece= "B con 35% de descuento";
+//         }else if(GrupoB.checked == true){
+//                     grupoPertenece= "B con 35% de descuento";
                         
-                }else if(GrupoC.checked == true){
-                            grupoPertenece= "C";
+//                 }else if(GrupoC.checked == true){
+//                             grupoPertenece= "C";
                             
-                        }else if(GrupoD.checked == true){
-                                grupoPertenece= "D";}
+//                         }else if(GrupoD.checked == true){
+//                                 grupoPertenece= "D";}
 
 
 
-    }else{
-        siSisben = "No";
-        grupoPertenece = "No aplica"
-    }
+//     }else{
+//         siSisben = "No";
+//         grupoPertenece = "No aplica"
+//     }
 
 
 
                                     
                                         
-    console.log(grupoPertenece)
+//     console.log(grupoPertenece)
 
-let documentoPDF = new jsPDF();
-documentoPDF.setFontSize(20);
-documentoPDF.text(70, 15, 'Factura por Hospitalización');
-documentoPDF.setFontSize(12);
-documentoPDF.text(20, 30, 'Fecha: ' + new Date());
-documentoPDF.setFontSize(15);
-documentoPDF.text(20, 40, 'Paciente: ' + nombre);
-documentoPDF.setFontSize(10);
-documentoPDF.text(20, 50, 'Tipo de Documento: ' + tipo_documento);
-documentoPDF.text(90, 50, 'Número de documento: ' + numero_documento);
-documentoPDF.text(60, 60, 'Motivo de Hospitalización: ' + motivo_hospitalizacion);
-documentoPDF.text(20, 70, 'Cuenta con Sisben: ' + siSisben); 
-documentoPDF.text(80, 70, 'Grupo al que Pertenece: ' + grupoPertenece);
-documentoPDF.text(20, 80, 'Fecha de Ingreso ' + fechainicio); 
-documentoPDF.text(80, 80, 'Fecha de Salida ' + fechaFin); 
-documentoPDF.text(80, 100, 'Valor a pagar ' + calcularValor()); 
-documentoPDF.save('Factura_Hospitalizacion');
+// let documentoPDF = new jsPDF();
+// documentoPDF.setFontSize(20);
+// documentoPDF.text(70, 15, 'Factura por Hospitalización');
+// documentoPDF.setFontSize(12);
+// documentoPDF.text(20, 30, 'Fecha: ' + new Date());
+// documentoPDF.setFontSize(15);
+// documentoPDF.text(20, 40, 'Paciente: ' + nombre);
+// documentoPDF.setFontSize(10);
+// documentoPDF.text(20, 50, 'Tipo de Documento: ' + tipo_documento);
+// documentoPDF.text(90, 50, 'Número de documento: ' + numero_documento);
+// documentoPDF.text(60, 60, 'Motivo de Hospitalización: ' + motivo_hospitalizacion);
+// documentoPDF.text(20, 70, 'Cuenta con Sisben: ' + siSisben); 
+// documentoPDF.text(80, 70, 'Grupo al que Pertenece: ' + grupoPertenece);
+// documentoPDF.text(20, 80, 'Fecha de Ingreso ' + fechainicio); 
+// documentoPDF.text(80, 80, 'Fecha de Salida ' + fechaFin); 
+// documentoPDF.text(80, 100, 'Valor a pagar ' + calcularValor()); 
+// documentoPDF.save('Factura_Hospitalizacion');
 
-}
+// }
